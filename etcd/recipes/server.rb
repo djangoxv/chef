4701130bce1891818f32aab54c2e90898eee39d7
djangoxv <djangoxv@gmail.com>
2016-06-09 16:48:32 -0700

    Simplifying recipe

--- etcd/recipes/server.rb
+++ etcd/recipes/server.rb
@@ -1,5 +1,10 @@
+
 etcd_installation 'default' do
   action :create
+end
+
+etcd_service_manager_upstart 'default' do
+  action :start
 end
 
 etcd_key "foo" do
@@ -7,17 +12,3 @@ etcd_key "foo" do
   action :set
 end
 
-remote_file '/tmp/foo' do
-  source 'http://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem'
-  action :nothing
-end
-
-http_request 'HEAD http://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem' do
-  message ''
-  url 'http://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem'
-  action :head
-  if File.exist?('/tmp/foo')
-    headers 'If-Modified-Since' => File.mtime('/tmp/foo').httpdate
-  end
-  notifies :create, 'remote_file[/tmp/foo]', :immediately
-end
