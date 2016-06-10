f46741915cc8485d367b61a27697633f04476539
djangoxv <djangoxv@gmail.com>
2016-06-09 21:37:02 -0700

    Adding rc back

---
+++ rc/metadata.rb
@@ -0,0 +1,18 @@
+name 'rc'
+maintainer 'John Bellone'
+maintainer_email 'jbellone@bloomberg.net'
+license 'Apache 2.0'
+description 'Library cookbook which provides a resource for writing runtime configuration files.'
+long_description 'Library cookbook which provides a resource for writing runtime configuration files.'
+version '1.6.4'
+
+supports 'ubuntu'
+supports 'centos'
+supports 'redhat'
+supports 'windows'
+supports 'freebsd'
+supports 'aix'
+supports 'solaris2'
+
+depends 'poise', '~> 2.2'
+depends 'line', '~> 0.6'
---
+++ rc/test/unit/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'chefspec'
+require 'chefspec/berkshelf'
+require 'toml'
+require 'java-properties'
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
