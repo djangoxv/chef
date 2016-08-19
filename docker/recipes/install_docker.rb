1f4342f9daa66460aab3be113160ad2aaa3cde44
Jeremy Capps <djangoxv@gmail.com>
2016-08-19 04:59:51 +0000

    adding service manager

--- docker/recipes/install_docker.rb
+++ docker/recipes/install_docker.rb
@@ -1,5 +1,10 @@
+recipe[chef-apt-docker]
 
 docker_installation_package 'default' do
   action :create
+  package_options %q|--force-yes -o Dpkg::Options::='--force-all'|
 end
 
+docker_service_manager_execute 'default' do
+  action :start
+end
